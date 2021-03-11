93bcf6d65f04ce0e23abd9b5947ebd7accf40888
pongsakorn <78690934+pongsakornsutana@users.noreply.github.com>
2021-03-11 22:32:58 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace home_weak_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Mode :");
+            string a;
+            double x, y;
+            a = Console.ReadLine();
+            x = double.Parse(Console.ReadLine());
+            y = double.Parse(Console.ReadLine());
+            if (x >= 0)
+            {
+                switch (a)
+                {
+                    case "time":
+
+                        y = Math.Pow(x - 1, 2);
+                        Console.WriteLine("Time :{0}", x);
+                        Console.WriteLine("Price :{0}", y);
+                        break;
+                    case "price":
+
+                        x = Math.Sqrt(y) + 1;
+                        Console.WriteLine("Time :{0}", x);
+                        Console.WriteLine("Price :{0}", y);
+                        break;
+                    default:
+                        Console.WriteLine("Invalid mode");
+                        break;
+                }
+            }
+            else if (x < 0)
+            {
+                Console.WriteLine("Invalid mouse position");
+            }
+
+        }
+    }
+}
+    
+            
+        
+
+        
+    
+
+
+        
+        
+
